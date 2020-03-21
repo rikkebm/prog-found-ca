971abf6816193ef0985d276a715b73cc1fadadb7
rikkebm <60385097+rikkebm@users.noreply.github.com>
2020-03-21 13:51:51 +0100

    Create level-1.js

---
+++ level-1.js
@@ -0,0 +1,83 @@
+//question 1
+
+//var dog = "Fido";
+
+//question 2 
+
+//var person = {
+//  name: "Rikke",
+//  age: 23
+//  }
+
+// question 3
+
+//var outOfStock = true;
+//   if(outOfStock === true) {
+//   console.log("Out of stock");
+//   }
+//   else {
+//   console.log("In stock");
+//   }
+
+//question 4
+
+//var numberArray = [10, 20, 30, 40, 50];
+//   for (var i=0; i <numberArray.length; i++){
+//   console.log(numberArray[i]);
+     }
+
+//question 5
+
+//for(var countLoop = 15; countLoop <= 25; countLoop++) {
+//  console.log(countLoop);
+//  }
+
+// question 6
+
+//for (var countLoop = 15; countLoop <= 25; countLoop++)
+//  if countLoop == 20)
+//  console.log(countLoop);
+
+// question 7
+
+//var dogArray = [
+//  {
+//  name: "Fido",
+//  age: 12,
+//  male: true,
+//  },
+//  {
+//  name: "Fifi",
+//  age: 8,
+//  male: false,
+//  }
+//  ];
+//  for (var i=0; i <dogArray.length; i++){
+//    console.log(dogArray[i].age);
+//    console.log(dogArray[i].male);
+//  }
+
+// question 8
+
+// function whatIDontLike(dislikeArgument){
+//     console.log("I don´t like" + dislikeArgument);
+//  }
+// whatIDontLike(" tomatoes");
+
+// question 9
+
+// function SubtractArgument(firstArgument, secondArgument) {
+//    var sum = firstArgument - secondArgument;
+//    console.log(sum);
+//  }
+//  SubtractArgument(30, 20);
+
+//  question 10
+
+//  var emptyArray = [];
+//   function AddArray(Argument){
+//     var sum = emptyArray + AddArray;
+//  }
+//  AddArray(40);
+//  console.log(emptyArray);
+
